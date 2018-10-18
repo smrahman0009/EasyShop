5e08c782885495af46fe593f8e3701919dbc7c80
Mushfikur Rahman <smrahman0009@gmail.com>
2018-10-18 21:10:24 +0600

    all the html file converted with html form

---
+++ admin.php
@@ -0,0 +1,92 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+<head>
+
+<title>WEB PAGE TITLE GOES HERE</title>
+
+</head>
+
+<body style="margin: 0px; padding: 0px; font-family: 'Trebuchet MS',verdana;">
+
+<table width="100%" style="height: 100%;" cellpadding="10" cellspacing="0" border="0">
+<tr>
+	<!-- ============ HEADER SECTION ============== -->
+	<td colspan="3" style="height: 100px;" bgcolor="green">
+		<h1>Website Logo</h1>
+		<h1 align="center" style="color: orange;"> EASY SHOP</h1>
+	</td>
+</tr>
+
+
+<!-- ============ NAVIGATION BAR SECTION ============== -->
+<tr>
+	<td colspan="4" valign="middle" height="30" bgcolor=" #6ab47b">
+		<table  width="100%">
+			<tr style="color: orange" align="center">
+				<td>
+					<a href="admin.php">
+						<input type="button" name="admin_button" value="ADMIN">
+					</a>
+				</td>
+				<td width="100%" align="left">
+					<a href="index.php">
+						<input type="button" name="home_button" value="HOME">
+					</a>
+				</td>
+				<td>
+					<a href="signin.php">
+						<input type="button" name="sign_in_button" value="sign in">
+					</a>
+				</td>
+				<td>
+					<a href="login.php">
+							<input type="button" name="log_in_button" value="log in">
+					</a>
+				</td>
+			</tr>
+	
+		</table>
+		
+	</td>
+</tr>
+
+<tr>
+	<!-- ============ LEFT COLUMN (MENU) ============== -->
+	<td width="20%" valign="top" bgcolor="#f5f5f5">
+		<h3>Catagory</h3>
+		<ul style="list-style: none;">
+			
+			<li><a href="#">Mens Wear</a> </li>
+			<li><a href="#">Womens Wear</a></li>
+			<li><a href="#">Kids</a> </li>
+			<li><a href="#">Gadgets</a></li>
+		</ul>
+	</td>
+
+	<!-- ============ MIDDLE COLUMN (CONTENT) ============== -->
+	<td width="55%" valign="top" bgcolor="#d2d8c7">
+
+		<h2>Users & Products Info: </h2>
+
+		<p>
+			This section will only be accessible by admin. Admin can view about products & registered customers details here.
+		</p>
+		<br>
+
+
+	</td>
+
+	<!-- ============ RIGHT COLUMN (CONTENT) ============== -->
+	<td width="25%" valign="top" bgcolor="#f5f5f5">&nbsp;</td>
+
+</tr>
+
+<!-- ============ FOOTER SECTION ============== -->
+<tr><td colspan="3" align="center" height="20" bgcolor="#599a68">Copyright ©</td></tr>
+</table>
+</body>
+
+<html>
+
+
