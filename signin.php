16b13f7b79a3e0905b81a32339286906c424d6c0
Mushfikur Rahman <35715315+smrahman0009@users.noreply.github.com>
2018-10-25 10:57:09 +0600

    use php include function

--- signin.php
+++ signin.php
@@ -1,78 +1,19 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-
-<html>
-<head>
-
-<title>WEB PAGE TITLE GOES HERE</title>
-<style>
-	.error {
-		color: #FF0000;
-	}
-</style>
-
-</head>
-
-<body style="margin: 0px; padding: 0px; font-family: 'Trebuchet MS',verdana;">
-
 <?php
 	session_start();
 	$first_name_er = $last_name_er = $email_er = $password_er = $confirm_password_er="";
 ?>
 
-<table width="100%" style="height: 100%;" cellpadding="10" cellspacing="0" border="0">
-<tr>
-	<!-- ============ HEADER SECTION ============== -->
-	<td colspan="3" style="height: 100px;" bgcolor="green">
-		<h1>Website Logo</h1>
-		<h1 align="center" style="color: orange;"> EASY SHOP</h1>
-	</td>
-</tr>
-
-
+<?php
+	include 'include/header.php';
+?>
 <!-- ============ NAVIGATION BAR SECTION ============== -->
-<tr>
-	<td colspan="3" valign="middle" height="30" bgcolor=" #6ab47b">
-		<table  width="100%">
-			<tr style="color: orange" align="center">
-				<td>
-					<a href="admin.php">
-						<input type="button" name="admin_button" value="ADMIN">
-					</a>
-				</td>
-				<td width="100%" align="LEFT">
-					<a href="index.php">
-						<input type="button" name="home_button" value="HOME">
-					</a>
-				</td>
-				<td>
-					<a href="signin.php">
-						<input type="button" name="sign_in_button" value="sign in">
-					</a>
-				</td>
-				<td>
-					<a href="login.php">
-							<input type="button" name="log_in_button" value="log in">
-					</a>
-				</td>
-			</tr>
-	
-		</table>
-		
-	</td>
-</tr>
-
-<tr>
+<?php
+	include 'include/navigation_bar.php';
+?>
 	<!-- ============ LEFT COLUMN (MENU) ============== -->
-	<td width="20%" valign="top" bgcolor="#f5f5f5">
-		<h3>Catagory</h3>
-		<ul style="list-style: none;">
-			
-			<li><a href="#">Mens Wear</a> </li>
-			<li><a href="#">Womens Wear</a></li>
-			<li><a href="#">Kids</a> </li>
-			<li><a href="#">Gadgets</a></li>
-		</ul>
-	</td>
+<?php
+	include 'include/left_col_menu.php';
+?>
 
 	<!-- ============ MIDDLE COLUMN (CONTENT) ============== -->
 	<!-- action="<?php $_SERVER["PHP_SELF"] ?>" -->
@@ -143,15 +84,12 @@
 	</td>
 
 	<!-- ============ RIGHT COLUMN (CONTENT) ============== -->
-	<td width="25%" valign="top" bgcolor="#f5f5f5">&nbsp;</td>
-
-</tr>
-
+<?php
+	include 'include/right_col_content.php';
+?>
 <!-- ============ FOOTER SECTION ============== -->
-<tr><td colspan="3" align="center" height="20" bgcolor="#599a68">Copyright ©</td></tr>
-</table>
-</body>
-
-<html>
+<?php
+	include 'include/footer.php';
+?>
 
 
