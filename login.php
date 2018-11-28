e77e19781dbc7b64045874517df231a250a90228
Mushfikur Rahman <35715315+smrahman0009@users.noreply.github.com>
2018-11-28 11:56:26 +0600

    Js validation complete

--- login.php
+++ login.php
@@ -44,7 +44,7 @@ if(isset($_REQUEST["error"]))//echo $_REQUEST["error"];
 			break;
 		}
 		else {
-			if (empty($_POST["u_email"])  && empty($_POST["password"])) {
+			if (empty($_POST["u_email"])  || empty($_POST["password"])) {
 			$un_auth_login = "invalid user name or password";
 		}
 		else{
@@ -58,12 +58,12 @@ if(isset($_REQUEST["error"]))//echo $_REQUEST["error"];
 ?>
 	<!-- ============ MIDDLE COLUMN (CONTENT) ============== -->
 	<td width="55%" valign="top" bgcolor="#d2d8c7">
-		<form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>">
+		<form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>" >
 			<table align="center">
 				<tr >
 					<td>
 						<p>User Id</p>
-						<input type="text" name="u_email">
+						<input type="text" name="u_email" id="email">
 					</td>
 				</tr>
 				<tr>
@@ -71,12 +71,12 @@ if(isset($_REQUEST["error"]))//echo $_REQUEST["error"];
 						<p>
 						Password
 						</p>
-						<input type="password" name="password">
+						<input type="password" name="password" id="password">
 					</td>
 				</tr>
 				<tr>
 					<td>
-						<input type="submit" name="lg_button" value="log in">
+						<input type="submit" name="lg_button" value="Login" onclick="validate();">
 					</td>
 					<td>
 						Forgot <a href="#">Password</a>
@@ -95,7 +95,36 @@ if(isset($_REQUEST["error"]))//echo $_REQUEST["error"];
 				</tr>
 			</table>
 		</form>
+<script>
+	function validate(){
+		
+		///////////////// EMAIL //////////////////////////
+		var email = document.getElementById("email").value;
+		var email_reg = /^[^@]+@[^@.]+\.[a-z]+$/i;
+		var email_reg_result = email_reg.test(email);
 
+
+		/////////////// Validate Password ////////////////////////
+		/////////////// Must contain a lowercase, uppercase letter and a number//
+		var password = document.getElementById("password").value;  
+		var password_reg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$/;
+		var password_reg_result = password_reg.test(password);
+
+		if (email == "" || password =="") {
+			alert("empty user name or password");
+			return false;
+		}
+		else if (email_reg_result == false) {
+			alert("Shoul be valid email address");
+			return false;
+		}
+		else if (password_reg_result == false) {
+			alert("invalid password");
+			return false;
+		}
+		else alert("valid data");
+	}
+</script>
 	</td>
 
 	<!-- ============ RIGHT COLUMN (CONTENT) ============== -->
